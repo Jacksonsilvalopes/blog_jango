74952a80dc6455e707431714d4d6a5944e0d909d
Jacksonsilvalopes <45312032+Jacksonsilvalopes@users.noreply.github.com>
2020-04-29 03:42:38 -0300

    template

--- blog/website/views.py
+++ blog/website/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+
+
 # Create your views here.
+def hello_blog(request):
+    lista = ['Djang', 'Python', 'Git', 'Html', 'banco de dados',
+     'linux', 'Nginx', 'Uwsgi', 'Systemactl']
+    data = {'name': 'Curso de Django 3', 'lista_tecnologia': lista}
+    return render(request, 'indexgeral.html', data)
